5d49aaf6d39eb61b98234cb15e43b928b1ec86e1
Phil213314 <49041196+Phil213314@users.noreply.github.com>
2021-10-19 23:50:43 +0300

    updated main script

--- repodl.py
+++ repodl.py
@@ -1,7 +1,4 @@
-try:
-	import os
-except Exception:
-	exit("os module can not be loaded. This usually happens when running python in a sandboxed environment (e. g. pythonista). These kinds of environments are not supported.")
+import os, bz2
 try:
 	import requests
 except Exception:
@@ -28,22 +25,50 @@ if not os.path.exists("debs"):
 else:
 	if not os.path.isdir("debs"):
 		exit("There is a file named 'debs' in the current folder. Remove it in order to run this script")
+def cleanf():
+	if os.path.exists("Packages"):
+		os.remove("Packages")
+	if os.path.exists("Packages.bz2"):
+		os.remove("Packages.bz2")
+def parsePackages():
+	pkgf = open("Packages")
+	pkgc = pkgf.readlines()
+	pkgf.close()
+	debs = []
+	packages = []
+	versions = []
+	for i in range(len(pkgc)):
+		if pkgc[i].startswith("Filename: "):
+			debs.append(pkgc[i].replace("Filename: ", "").replace("\n", ""))
+		elif pkgc[i].startswith("Package: "):
+			packages.append(pkgc[i].replace("Package: ", "").replace("\n", ""))
+		elif pkgc[i].startswith("Version: "):
+			versions.append(pkgc[i].replace("Version: ", "").replace("\n", ""))
+	return [debs, packages, versions]
+print("Trying to download " + repourl + "Packages")
 dl(repourl + "Packages", "Packages")
-pkgf = open("Packages")
-pkgc = pkgf.readlines()
-pkgf.close()
-debs = []
-packages = []
-versions = []
-for i in range(len(pkgc)):
-	if pkgc[i].startswith("Filename: "):
-		debs.append(pkgc[i].replace("Filename: ", "").replace("\n", ""))
-	elif pkgc[i].startswith("Package: "):
-		packages.append(pkgc[i].replace("Package: ", "").replace("\n", ""))
-	elif pkgc[i].startswith("Version: "):
-		versions.append(pkgc[i].replace("Version: ", "").replace("\n", ""))
+out = parsePackages()
+debs = out[0]
+packages = out[1]
+versions = out[2]
+if debs == []:
+	print("File invalid.")
+	print("Trying to download " + repourl + "Packages.bz2")
+	os.remove("Packages")
+	dl(repourl + "Packages.bz2", "Packages.bz2")
+	open("Packages", 'wb').write(bz2.BZ2File("Packages.bz2").read())
+	out = parsePackages()
+	debs = out[0]
+	packages = out[1]
+	versions = out[2]
+	if debs == []:
+		print("File invalid.")
+		cleanf()
+		exit("Could not get Packages file.")
+print("Successfully got Packages file")
+#print("Debs: " + ", ".join(debs))
 if len(debs) != len(packages) or len(debs) != len(versions) or len(packages) != len(versions):
 	exit("'Packages' format error")
-#print("Debs: " + ", ".join(debs))
 for i in tqdm(range(len(debs))):
 	dl(repourl + debs[i], "debs/" + packages[i] + "-" + versions[i] + ".deb")
+cleanf()
